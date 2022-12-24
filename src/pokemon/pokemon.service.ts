357bb2457a19aeb58ac1bb1cdac1bbd3ecad9a71
Facundo Montenegro <facumonte29@gmail.com>
2022-12-24 00:37:40 -0300

    agrega find by..

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,6 +1,6 @@
 import { BadGatewayException, BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
@@ -47,9 +47,15 @@ export class PokemonService {
     }
 
     // Mongo id
-
+    if ( !pokemon && isValidObjectId(term)) {
+     pokemon = await this.pokemonModel.findById(term);
+    }
 
     // Name
+    if (!pokemon){
+      pokemon = await this.pokemonModel.findOne({name: term.toLowerCase().trim()});
+    }
+
 
     if (!pokemon){
       throw new NotFoundException(`Pokemon with id, name or no  "${term}" not found `);
